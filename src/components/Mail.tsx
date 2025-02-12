71bacddad165f0f3a3b290ef818688d535148a83
juannpg <juan.pasgut.workspace@gmail.com>
2025-02-12 10:00:33 +0100

    github icon / justify in item / modal transition time / github button / gap in projects

--- src/components/Mail.tsx
+++ src/components/Mail.tsx
@@ -13,9 +13,9 @@ export default function Mail() {
 
   return (
     <>
-      <div className="w-80 flex justify-center items-center bg-gray-300 rounded-lg text-black absolute top-10 p-2 z-10 transition-all duration-300 ease-in-out"
+      <div className="w-80 flex justify-center items-center bg-gray-300 rounded-lg text-black fixed top-10 p-2 z-10 transition-all duration-500 ease-in-out"
         style={{
-          opacity: modal ? '1' : '0'
+          opacity: modal ? '0.95' : '0'
         }}
       >
         <p>Email copiado al portapapeles</p>
