abccae9adf1cdfdd1bcb8087865a7d4e4b6f4f32
juannpg <juan.pasgut.workspace@gmail.com>
2025-02-15 14:32:30 +0100

    translate

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), astroI18next()]
 });
